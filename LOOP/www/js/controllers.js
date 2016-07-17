30d183af062b332e0d96c795bb92e14ac2849aa8
Leong Wei Kong <wkleong.2014@sis.smu.edu.sg>
2016-07-17 14:20:31 +0800

    Added Link for Start Activity from Cycle to Inprogress
    Current Bug - Unable to link properly
    Steps to replicate:
    Click on Start Activity
    Click on Profile Tab
    Click on Cycle Tab
    Click on Start Activity Again
    Click on Cycle Tab (will have bug)

--- LOOP/www/js/controllers.js
+++ LOOP/www/js/controllers.js
@@ -64,7 +64,7 @@ angular.module('app.controllers', [])
 
 })
 
-.controller('cycleCtrl', function($scope, leafletData) {
+.controller('cycleCtrl', function($scope, $state, leafletData) {
     leafletData.getMap().then(function (map) {
         map.locate({ setView: true, maxZoom: 22, watch: true, enableHighAccuracy: true });
         map.on('locationfound', function (e) {
@@ -203,124 +203,128 @@ angular.module('app.controllers', [])
       defaults: {
         scrollWheelZoom: false
       }
-  });
+    });
 
-  $scope.start = function () {
-      leafletData.getMap().then(function (map) {
-          map.stopLocate();
-          map.locate({ setView: true, maxZoom: 22, watch: true, enableHighAccuracy: true });
-          map.on('locationfound', function (e) {
-              //$scope.markers.currentLocation.lat = e.latlng.lat;
-              //$scope.markers.currentLocation.lng = e.latlng.lng;
-              //$scope.markers.currentLocation.message = "current position";
-              //$scope.markers.currentLocation.focus = true;
-              //$scope.markers.currentLocation.lng = e.latlng.lng
-              $scope.paths.currentLoc.latlngs = [];
-              $scope.paths.currentLoc.latlngs.push(e.latlng.lat);
-              $scope.paths.currentLoc.latlngs.push(e.latlng.lng);
-              $scope.paths.p1.latlngs.push({lat: e.latlng.lat, lng: e.latlng.lng});
-              $scope.paths.p1.coordinates.push({ //storing each coordinate information
-                  lat: e.latlng.lat,
-                  lng: e.latlng.lng,
-                  alt: e.altitude,
-                  time: e.timestamp
-              });
-              $scope.paths.toGEOJson.coordinates.push([e.latlng.lat, e.latlng.lng]);
-              alert("next GPS Ping");
-          });
-      });
-  };
+    $scope.startActivity = function () {
+        $state.go('inprogress');
+    }
+
+    $scope.start = function () {
+        leafletData.getMap().then(function (map) {
+            map.stopLocate();
+            map.locate({ setView: true, maxZoom: 22, watch: true, enableHighAccuracy: true });
+            map.on('locationfound', function (e) {
+                //$scope.markers.currentLocation.lat = e.latlng.lat;
+                //$scope.markers.currentLocation.lng = e.latlng.lng;
+                //$scope.markers.currentLocation.message = "current position";
+                //$scope.markers.currentLocation.focus = true;
+                //$scope.markers.currentLocation.lng = e.latlng.lng
+                $scope.paths.currentLoc.latlngs = [];
+                $scope.paths.currentLoc.latlngs.push(e.latlng.lat);
+                $scope.paths.currentLoc.latlngs.push(e.latlng.lng);
+                $scope.paths.p1.latlngs.push({lat: e.latlng.lat, lng: e.latlng.lng});
+                $scope.paths.p1.coordinates.push({ //storing each coordinate information
+                    lat: e.latlng.lat,
+                    lng: e.latlng.lng,
+                    alt: e.altitude,
+                    time: e.timestamp
+                });
+                $scope.paths.toGEOJson.coordinates.push([e.latlng.lat, e.latlng.lng]);
+                alert("next GPS Ping");
+            });
+        });
+    };
     /*
-     $http({
-      url: user.update_path,
-      method: "POST",
-      headers: {
-               'Content-Type': undefined
+        $http({
+        url: user.update_path,
+        method: "POST",
+        headers: {
+                'Content-Type': undefined
             },
-      data: {user_id: user.id, draft: true}
-     });
+        data: {user_id: user.id, draft: true}
+        });
     */
 
-  $scope.dbConnect = function () {
-      $http({
-          method: 'GET',
-          url: 'http://backendpgsql-ywk93.rhcloud.com/main', //?firstname=ong
-          params: {firstname : 'ong'}
-      }).then(function successCallback(response) {
-          var test = response.data;
-          for(var properties in test){
-              alert(properties); //prints the properties
-              alert(test[properties]); //prints the value
-          }
-          //****Accessing Individual Properties****
-          //alert(test["firstName"]);
-          //alert(test.firstName);
-          //***************************************
+    $scope.dbConnect = function () {
+        $http({
+            method: 'GET',
+            url: 'http://backendpgsql-ywk93.rhcloud.com/main', //?firstname=ong
+            params: {firstname : 'ong'}
+        }).then(function successCallback(response) {
+            var test = response.data;
+            for(var properties in test){
+                alert(properties); //prints the properties
+                alert(test[properties]); //prints the value
+            }
+            //****Accessing Individual Properties****
+            //alert(test["firstName"]);
+            //alert(test.firstName);
+            //***************************************
 
-      }, function errorCallback(response) {
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
-      });
-  };
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    };
 
-  $scope.end = function () {
-      leafletData.getMap().then(function (map) {
-          map.stopLocate();
-          alert($scope.paths.toGEOJson);
-          alert($scope.paths.toGEOJson.type);
-          alert($scope.paths.toGEOJson.coordinates);
-          //var firstCoord = $scope.paths.p1.coordinates[0];
-          //var lastPosition = $scope.paths.p1.coordinates.length - 1;
-          //var lastCoord = $scope.paths.p1.coordinates[lastPosition];
-          //alert("lat: " + firstCoord["lat"] + " | " + "lng: " + firstCoord["lng"]);
-          //alert("lat: " + lastCoord["lat"] + " | " + "lng: " + lastCoord["lng"]);
-          var coords = $scope.paths.p1.coordinates;
-          var calculate = geolib.getPathLength($scope.paths.p1.latlngs);
-          var totalElevation = 0;
-          var avgElevation = 0;
-          for(var i=0;i<coords.length;i++){
-              var coord = coords[i];
-              totalElevation += coord["alt"];
-          }
-          avgElevation = totalElevation / coords.length;
-          alert("average elevation is: " + avgElevation);
-          alert("Total Cycle Distance: " + calculate + "m");
-          alert($scope.paths.p1.coordinates); //an array of hashtable [{ lat : 1.3, lng : 1.11111, alt: 123, time:1323232321 }, { lat : 1.3, lng : 1.11111, alt: 123, time:1323232321  }]
-          alert("You have ended your cycle!");
-      });
-  };
+    $scope.end = function () {
+        leafletData.getMap().then(function (map) {
+            map.stopLocate();
+            alert($scope.paths.toGEOJson);
+            alert($scope.paths.toGEOJson.type);
+            alert($scope.paths.toGEOJson.coordinates);
+            //var firstCoord = $scope.paths.p1.coordinates[0];
+            //var lastPosition = $scope.paths.p1.coordinates.length - 1;
+            //var lastCoord = $scope.paths.p1.coordinates[lastPosition];
+            //alert("lat: " + firstCoord["lat"] + " | " + "lng: " + firstCoord["lng"]);
+            //alert("lat: " + lastCoord["lat"] + " | " + "lng: " + lastCoord["lng"]);
+            var coords = $scope.paths.p1.coordinates;
+            var calculate = geolib.getPathLength($scope.paths.p1.latlngs);
+            var totalElevation = 0;
+            var avgElevation = 0;
+            for(var i=0;i<coords.length;i++){
+                var coord = coords[i];
+                totalElevation += coord["alt"];
+            }
+            avgElevation = totalElevation / coords.length;
+            alert("average elevation is: " + avgElevation);
+            alert("Total Cycle Distance: " + calculate + "m");
+            alert($scope.paths.p1.coordinates); //an array of hashtable [{ lat : 1.3, lng : 1.11111, alt: 123, time:1323232321 }, { lat : 1.3, lng : 1.11111, alt: 123, time:1323232321  }]
+            alert("You have ended your cycle!");
+        });
+    };
 
-  $scope.locate = function () {
-      var posOptions = { timeout: 10000, enableHighAccuracy: false };
-      $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
-          $scope.center.lat = position.coords.latitude;
-          $scope.center.lng = position.coords.longitude;
-          $scope.center.zoom = 17;
-          alert($scope.center.lat + '   ' + $scope.center.lng)
-      }, function (err) {
-          console.log(err)
-      });
-  };
+    $scope.locate = function () {
+        var posOptions = { timeout: 10000, enableHighAccuracy: false };
+        $cordovaGeolocation.getCurrentPosition(posOptions).then(function (position) {
+            $scope.center.lat = position.coords.latitude;
+            $scope.center.lng = position.coords.longitude;
+            $scope.center.zoom = 17;
+            alert($scope.center.lat + '   ' + $scope.center.lng)
+        }, function (err) {
+            console.log(err)
+        });
+    };
 
-  $scope.options = {
+    $scope.options = {
     loop: false,
     effect: 'slide',
-  }
+    }
 
-  $scope.$on("$ionicSlides.sliderInitialized", function(event, data){
+    $scope.$on("$ionicSlides.sliderInitialized", function(event, data){
     // data.slider is the instance of Swiper
     $scope.slider = data.slider;
-  });
+    });
 
-  $scope.$on("$ionicSlides.slideChangeStart", function(event, data){
+    $scope.$on("$ionicSlides.slideChangeStart", function(event, data){
     console.log('Slide change is beginning');
-  });
+    });
 
-  $scope.$on("$ionicSlides.slideChangeEnd", function(event, data){
+    $scope.$on("$ionicSlides.slideChangeEnd", function(event, data){
     // note: the indexes are 0-based
     $scope.activeIndex = data.activeIndex;
     $scope.previousIndex = data.previousIndex;
-  });
+    });
 })
 
 .controller('inprogressCtrl', function($scope, $log, $cordovaGeolocation, leafletData) {
