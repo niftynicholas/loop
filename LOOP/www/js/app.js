eb9587d77f769f8728bf214080a4aed7e315fdd4
Nicholas Tan Joo Sin <nicholas.tan.92@hotmail.com>
2016-07-19 16:28:27 +0800

    - Added cordova-plugin-android-permissions to ask for location permission.

--- LOOP/www/js/app.js
+++ LOOP/www/js/app.js
@@ -5,44 +5,79 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'leaflet-directive','timer'])
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'leaflet-directive', 'timer'])
 
 .filter('digits', function() {
     return function(input) {
         if (input < 10) input = '0' + input;
-      
+
         return input;
     }
 })
 
-.factory('dataShare', function () {
+.factory('dataShare', function() {
     var service = {};
     service.data = false;
-    service.sendData = function (data) {
+    service.sendData = function(data) {
         this.data = data;
     };
-    service.getData = function () {
+    service.getData = function() {
         return this.data;
     };
     return service;
 })
 
-.config(function ($ionicConfigProvider) {
-  $ionicConfigProvider.tabs.position('bottom');
-  $ionicConfigProvider.navBar.alignTitle('center')
+.config(function($ionicConfigProvider) {
+    $ionicConfigProvider.tabs.position('bottom');
+    $ionicConfigProvider.navBar.alignTitle('center')
 })
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
+    $ionicPlatform.ready(function() {
+        var permissions = cordova.plugins.permissions;
+        permissions.hasPermission(permissions.ACCESS_COARSE_LOCATION, checkPermissionCallback, null);
+
+        function checkPermissionCallback(status) {
+            if (!status.hasPermission) {
+                var errorCallback = function() {
+                    console.warn('Location permission is not turned on');
+                }
+
+                permissions.requestPermission(
+                    permissions.ACCESS_COARSE_LOCATION,
+                    function(status) {
+                        if (!status.hasPermission) errorCallback();
+                    },
+                    errorCallback);
+            }
+        }
+
+        /*permissions.hasPermission(permissions.ACCESS_FINE_LOCATION, checkPermissionCallback, null);
+
+        function checkPermissionCallback(status) {
+            if (!status.hasPermission) {
+                var errorCallback = function() {
+                    console.warn('Location permission is not turned on');
+                }
+
+                permissions.requestPermission(
+                    permissions.ACCESS_FINE_LOCATION,
+                    function(status) {
+                        if (!status.hasPermission) errorCallback();
+                    },
+                    errorCallback);
+            }
+        }*/
+
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleDefault();
+        }
+    });
 })
