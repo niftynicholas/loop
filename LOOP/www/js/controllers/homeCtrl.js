16b1e45da873fa15b59a4a0b155b8c4abf71c574
Leong Wei Kong <wkleong.2014@sis.smu.edu.sg>
2016-09-08 15:01:31 +0800

    - Edited one time load to homeCtrl as well

--- LOOP/www/js/controllers/homeCtrl.js
+++ LOOP/www/js/controllers/homeCtrl.js
@@ -2,66 +2,70 @@ angular.module('app.main.controllers')
 
 .controller('homeCtrl', function($scope, homeData, routeName, $state, $http, leafletData) {
     $scope.routes = homeData.getData();
-
+    $scope.firstLoad = true;
     $scope.$on("$ionicView.afterEnter", function() {
-        for (var i = 0; i < $scope.routes.length; i++) {
-            var counter = i;
-            var routeCID = $scope.routes[i].cid;
+        if($scope.firstLoad){
+            for (var i = 0; i < $scope.routes.length; i++) {
+                var counter = i;
+                var routeCID = $scope.routes[i].cid;
 
-            $.ajax({
-                dataType: 'json',
-                url: 'https://sgcycling-sgloop.rhcloud.com/api/cyclist/route/getRoute',
-                async: false,
-                method: 'POST',
-                data: {
-                    cid: routeCID,
-                    token: localStorage.getItem("token")
-                },
-                success: function(response) {
-                    var json = response;
-                    $scope.routes[counter]["geojson"] = json.route;
-                    $scope.routes[counter]["distance"] = json.distance;
-                    $scope.routes[counter]["duration"] = json.duration;
-                    $scope.routes[counter]["noOfComments"] = json.comments.length;
+                $.ajax({
+                    dataType: 'json',
+                    url: 'https://sgcycling-sgloop.rhcloud.com/api/cyclist/route/getRoute',
+                    async: false,
+                    method: 'POST',
+                    data: {
+                        cid: routeCID,
+                        token: localStorage.getItem("token")
+                    },
+                    success: function(response) {
+                        var json = response;
+                        $scope.routes[counter]["geojson"] = json.route;
+                        $scope.routes[counter]["distance"] = json.distance;
+                        $scope.routes[counter]["duration"] = json.duration;
+                        $scope.routes[counter]["noOfComments"] = json.comments.length;
 
-                    var coordinates = JSON.parse(response.route).coordinates;
+                        var coordinates = JSON.parse(response.route).coordinates;
 
-                    // Swap lat lng position for fitBounds()
-                    var temp = [];
-                    for (var i = 0; i < coordinates.length; i++) {
-                        var temp2 = [];
-                        temp2.push(coordinates[i][1]);
-                        temp2.push(coordinates[i][0]);
-                        temp.push(temp2);
+                        // Swap lat lng position for fitBounds()
+                        var temp = [];
+                        for (var i = 0; i < coordinates.length; i++) {
+                            var temp2 = [];
+                            temp2.push(coordinates[i][1]);
+                            temp2.push(coordinates[i][0]);
+                            temp.push(temp2);
+                        }
+
+                        coordinates = temp;
+
+                        var geojson = JSON.parse(json.route);
+                        var myStyle = {
+                            weight: 8,
+                            opacity: 1,
+                            color: '#022F40'
+                        }
+
+                        leafletData.getMap((routeCID)).then(function(map) {
+                            map.fitBounds(
+                                coordinates, {
+                                    animate: true,
+                                    reset: true,
+                                    padding: [25, 25],
+                                    maxZoom: 16
+                                }
+                            );
+                            map.invalidateSize();
+
+                            L.geoJson(geojson, {
+                                style: myStyle
+                            }).addTo(map);
+                        })
                     }
-
-                    coordinates = temp;
-
-                    var geojson = JSON.parse(json.route);
-                    var myStyle = {
-                        weight: 8,
-                        opacity: 1,
-                        color: '#022F40'
-                    }
-
-                    leafletData.getMap((routeCID)).then(function(map) {
-                        map.fitBounds(
-                            coordinates, {
-                                animate: true,
-                                reset: true,
-                                padding: [25, 25],
-                                maxZoom: 16
-                            }
-                        );
-                        map.invalidateSize();
-
-                        L.geoJson(geojson, {
-                            style: myStyle
-                        }).addTo(map);
-                    })
-                }
-            })
+                })
+            }
+            $scope.firstLoad = false;
         }
+
 
     });
 
